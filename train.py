3fd72c54cc6b3c2216fb32a8dd41c865c8a34e5b
wzp <zhipangwang@gmail.com>
2023-03-25 09:18:09 +0000

    update model

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ def train_main(config: CustomConfig):
     device = config.device
     os.environ['CUDA_VISIBLE_DEVICES'] = config.cuda_id
     
-    start_time = get_cur_time()
+    start_time = get_cur_time().replace(':', '_')
     config.version = config.version.replace(' ', '_')
     saved_res_fold = path(config.save_res_fold) / path(f'{start_time}_{config.version}')
     saved_model_fold = saved_res_fold / path('saved_model')
